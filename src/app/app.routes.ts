81b7323e3772e28cf425815e54f987da83f29577
RanjaniKathiresan <ranjanikathiresan2012@gmail.com>
2024-06-23 17:57:39 +0530

    Created basic layout for the blog site

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SinglePostComponent } from './pages/single-post/single-post.component';
+import { SingleCategoryComponent } from './pages/single-category/single-category.component';
+import { TermsAndConditionComponent } from './pages/terms-and-condition/terms-and-condition.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'post', component: SinglePostComponent },
+  { path: 'category', component: SingleCategoryComponent },
+  // {path: 'about', component: AboutUs},
+  { path: 'terms-condition', component: TermsAndConditionComponent },
+  { path: 'contact', component: ContactUsComponent },
+];
